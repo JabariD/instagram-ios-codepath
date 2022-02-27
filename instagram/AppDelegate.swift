cf4108d9d32ee0be954c0428857cb74be7592d7c
Payton Dennis <dennispayton15@gmail.com>
2022-02-26 23:20:39 -0500

    Create Project and Init Parse

--- instagram/AppDelegate.swift
+++ instagram/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -14,6 +15,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "TlaK7aaeWSbDdRHbYRoawbJEAi0HxMqsos2bi0jl" // <- UPDATE
+                    $0.clientKey = "ATsYYQ2zZDroB2vQDVSZNdXm6Ewh2RE07BDF6MPk" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+            Parse.initialize(with: parseConfig)
+        
         return true
     }
 

13cf0154195c4f3dfa516229604aca58a2642c71
Payton Dennis <dennispayton15@gmail.com>
2022-02-27 17:15:33 -0500

    User can log out
    User can log out, when this action is performed they will be taken to the Login Screen. Also involves adding a Logout button!

--- instagram/FeedViewController.swift
+++ instagram/FeedViewController.swift
@@ -67,6 +67,16 @@ class FeedViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
 
+    @IBAction func onLogoutButton(_ sender: Any) {
+        // clear Parse cache and actually log out
+        PFUser.logOut()
+        print("The user has logged out!")
+        
+        let main = UIStoryboard(name: "Main", bundle: nil)
+        let loginViewController = main.instantiateViewController(withIdentifier: "LoginViewController")
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let delegate = windowScene.delegate as? SceneDelegate else { return }
+        delegate.window?.rootViewController = loginViewController
+    }
     /*
     // MARK: - Navigation
 
